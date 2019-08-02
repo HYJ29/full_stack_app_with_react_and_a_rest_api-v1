30371c6f44d0ff22f7e71398c45f7071882b411a
HYJ <teen14y@naver.com>
2019-08-02 21:07:49 +0900

    component layout

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,36 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import {withContext} from './Context';
+
+import Header from './components/Header';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import Courses from './components/Courses';
+import CreateCourse from './components/CreateCourse';
+import CourseDetail from './components/CourseDetail';
+import UpdateCourse from './components/UpdateCourse';
+
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
 
 
 export default ()=> (
   <Router>
-    <Switch>
-      <Route />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Courses}/>
+        <Route path='/courses/create' component={CreateCourse}/>
+        <Route exact path='/courses/:id' component={CourseDetail}/>
+        <Route path='/courses/:id/update' component={UpdateCourse}/>
+        <Route path='/signIn' component={UserSignIn}/>
+        <Route path='/signUp' component={UserSignUp}/>
+        <Route path='/signOut' component={UserSignOut}/>
+      </Switch>
+    </div>
+
   </Router>
 );
---
+++ client/src/components/UserSignOut.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+
+export default () => {
+  return (
+    <Redirect to='/'/>
+  )
+}
